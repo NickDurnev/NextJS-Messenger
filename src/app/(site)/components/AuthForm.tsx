d284dd2db94cd28f5e7a5a444ad607bcb14226d9
@FoxMykyta <nick_durnev@pragmasoft.com.ua>
2023-10-21 23:21:21 +0300

    feat: added prisma schema, inited mongo db collections

--- src/app/(site)/components/AuthForm.tsx
+++ src/app/(site)/components/AuthForm.tsx
@@ -2,14 +2,16 @@
 
 import { useCallback, useState } from "react";
 import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import { BsGithub, BsGoogle } from "react-icons/bs";
 import Input from "@/app/components/inputs/Input";
 import Button from '@/app/components/Button';
+import AuthSocialButton from "./AuthSocialButton";
 
 type Variant = "LOGIN" | "REGISTER";
 
 const AuthForm = () => {
     const [variant, setVariant] = useState<Variant>("LOGIN");
-    const [Loading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
     const toggleVariant = useCallback(() => {
         if (variant === "LOGIN") {
@@ -50,15 +52,36 @@ const AuthForm = () => {
             <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
                 <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
                     {variant === 'REGISTER' && (
-                        <Input id="name" label="Name" register={register} errors={errors} />
+                        <Input id="name" label="Name" register={register} errors={errors} disabled={isLoading} />
                     )}
-                    <Input id="email" label="Email adress" type="email" register={register} errors={errors} />
-                    <Input id="password" label="Password" type="password" register={register} errors={errors} />
+                    <Input id="email" label="Email adress" type="email" register={register} errors={errors} disabled={isLoading} />
+                    <Input id="password" label="Password" type="password" register={register} errors={errors} disabled={isLoading} />
                     <div>
                         <Button disabled={isLoading} fullWidth type="submit">{variant === "LOGIN" ? "Sign in" : "Register"}</Button>
+                    </div>
+                    <div className="mt-6">
+                        <div className="relative">
+                            <div className="absolute inset-0 flex items-center">
+                                <div className="w-full border-t border-gray-300">
+                                    <div className="relative flex justify-center text-sm">
+                                        <span className="bg-white px-2 text-gray-500">
+                                            Or continue with
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className="mt-6 flex gap-2">
+                                    <AuthSocialButton icon={BsGithub} onClick={() => socialAction("github")} />
+                                    <AuthSocialButton icon={BsGoogle} onClick={() => socialAction("google")} />
+                                </div>
+                            </div>
+                            <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">{variant === "LOGIN" ? "New to Messenger?" : "Already have an account?"}</div>
+                            <div onClick={toggleVariant} className="underline cursor-pointer">{variant === 'LOGIN' ? "Create an account" : "Login"}</div>
+                        </div>
                     </div>
                 </form>
             </div>
         </div>
     )
 }
+
+export default AuthForm;
---
+++ src/app/(site)/components/AuthSocialButton.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+import { IconType } from "react-icons";
+
+interface AuthSocialButtonprops {
+    icon: IconType;
+    onClick: () => void;
+}
+
+const AuthSocialButton: FC<AuthSocialButtonprops> = ({
+    icon: Icon,
+    onClick,
+}) => {
+    return (
+        <button
+            type="button"
+            onClick={onClick}
+            className="inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0"
+        ></button>
+    );
+};
+
+export default AuthSocialButton;
