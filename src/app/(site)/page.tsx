601b4af3e880a589ae45ae033405e284bf5411a2
NickDurnev <nikitaspec@gmail.com>
2024-01-04 23:47:02 +0200

    feat: added hero title to home page

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,13 +1,20 @@
-import Hero from '../components/Hero';
-import UserMenu from './components/UserMenu/UserMenu';
+import Hero from "../components/Hero";
+import UserMenu from "./components/UserMenu/UserMenu";
 
 export default function Home() {
   return (
     <main className="relative bg-blue-950">
-      <section className='w-full h-20 px-10 py-4 '>
+      <div className="w-full flex justify-center">
+        <div className="w-full sm:w-[70%] md:w-[50%] absolute top-20 sm:top-4 text-center">
+          <h1 className="title">
+            Be in touch with <span>Connectify</span>
+          </h1>
+        </div>
+      </div>
+      <section className="w-full h-20 px-10 py-4 ">
         <UserMenu />
       </section>
       <Hero />
-    </main >
-  )
+    </main>
+  );
 }
