e1b5dc97ffec09e47cbc2b76608febde58957b16
NickDurnev <nikitaspec@gmail.com>
2024-01-13 17:15:34 +0200

    fix: debugging

--- src/app/actions/getSession.ts
+++ src/app/actions/getSession.ts
@@ -4,7 +4,7 @@ import { cookies } from "next/headers";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 
 const getSession = async () => {
-  console.log("COOKIE", cookies().get("next-auth.session-token"));
+  console.log("COOKIE", cookies().get("__Secure-next-auth.session-token"));
   return await getServerSession(authOptions);
 };
 
