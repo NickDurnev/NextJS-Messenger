ecf28b13ed4f7e5a593e8de3c551372186ed9734
NickDurnev <nikitaspec@gmail.com>
2024-01-14 16:04:12 +0200

    fix: add option to asios instance

--- src/app/libs/axios.ts
+++ src/app/libs/axios.ts
@@ -4,8 +4,10 @@ const BASE_APP_URL = process.env.NEXT_PUBLIC_BASE_APP_URL;
 export default axios.create({
   baseURL: `${BASE_APP_URL}api/`,
   headers: { "Content-Type": "application/json" },
+  withCredentials: true,
 });
 export const axiosAuth = axios.create({
   baseURL: `${BASE_APP_URL}api/`,
   headers: { "Content-Type": "application/json" },
+  withCredentials: true,
 });
