6cc05bf2367024838c08bbb4a4d15bdab75c414f
NickDurnev <nikitaspec@gmail.com>
2024-11-12 16:34:44 +0200

    feat: added test manifest.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,23 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google';
-import { getServerSession } from 'next-auth';
-import ToasterContext from './context/ToasterContext';
-import AuthContext from './context/AuthContext';
-import AppContainer from './components/AppContainer';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { getServerSession } from "next-auth";
+import ToasterContext from "./context/ToasterContext";
+import AuthContext from "./context/AuthContext";
+import AppContainer from "./components/AppContainer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Messenger',
-  description: 'Messenger',
-}
+  manifest: "/manifest.json",
+  title: "Connectify",
+  description: "Messenger",
+};
 
 export default async function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   const session = await getServerSession();
 
@@ -25,11 +26,9 @@ export default async function RootLayout({
       <body className={inter.className}>
         <AuthContext session={session}>
           <ToasterContext />
-          <AppContainer>
-            {children}
-          </AppContainer>
+          <AppContainer>{children}</AppContainer>
         </AuthContext>
       </body>
     </html>
-  )
+  );
 }
